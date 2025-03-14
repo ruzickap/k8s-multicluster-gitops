ea5a949b3f2f695383687d37c4b3e5a88ad20290
Petr Ruzicka <petr.ruzicka@gmail.com>
2025-03-14 16:43:45 +0100

    feat: initial mise files and tests

--- README.md
+++ README.md
@@ -2,3 +2,25 @@
 
 Infrastructure as Code for provisioning multiple Kubernetes clusters, managed
 using GitOps with ArgoCD
+
+Tests:
+
+```bash
+SOPS_AGE_KEY="$(grep -v ^# ~/Documents/secrets/age.txt)"
+export SOPS_AGE_KEY
+MISE_SOPS_AGE_KEY="$(grep -v ^# ~/Documents/secrets/age.txt)"
+export MISE_SOPS_AGE_KEY
+
+docker run --rm -it \
+  --env SOPS_AGE_KEY --env MISE_SOPS_AGE_KEY \
+  -v "$PWD:/mnt" \
+  -v "/var/run/docker.sock:/var/run/docker.sock" \
+  --workdir /mnt \
+  bash bash -c 'set -euxo pipefail && \
+    apk add docker && \
+    wget https://mise.run -O - | sh && \
+    eval "$(~/.local/bin/mise activate bash)" && \
+    mise run "create:*:*" && \
+    mise run "delete:*:*" \
+  '
+```
---
+++ scripts/run-kind.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+
+# The "create" needs to be idempotent !
+create() {
+  if kind get clusters 2>&1 | grep -q "^${CLUSTER_FQDN}$"; then
+    echo "*** Cluster \"${CLUSTER_FQDN}\" already exists...."
+  else
+    mkdir -p "${CLUSTERS_KUBECONFIG_DIRECTORY}"
+    cat << EOF | kind create cluster --name "${CLUSTER_FQDN}" --kubeconfig "${CLUSTERS_KUBECONFIG_DIRECTORY}/kubeconfig_${CLUSTER_FQDN}.yml" --config -
+kind: Cluster
+apiVersion: kind.x-k8s.io/v1alpha4
+nodes:
+- role: control-plane
+- role: worker
+EOF
+  fi
+}
+
+delete() {
+  if kind get clusters | grep -q "^${CLUSTER_FQDN}$"; then
+    kind delete cluster --name "${CLUSTER_FQDN}" --kubeconfig "${CLUSTERS_KUBECONFIG_DIRECTORY}/kubeconfig_${CLUSTER_FQDN}.yml"
+    if [[ -f "${CLUSTERS_KUBECONFIG_DIRECTORY}/kubeconfig_${CLUSTER_FQDN}.yml" ]]; then
+      echo "*** Deleting \"${CLUSTERS_KUBECONFIG_DIRECTORY}/kubeconfig_${CLUSTER_FQDN}.yml\" ..."
+      rm "${CLUSTERS_KUBECONFIG_DIRECTORY}/kubeconfig_${CLUSTER_FQDN}.yml"
+    fi
+    if [[ -d "${CLUSTERS_KUBECONFIG_DIRECTORY}" && -z "$(ls -A "${CLUSTERS_KUBECONFIG_DIRECTORY}")" ]]; then
+      echo "*** Deleting empty \"${CLUSTERS_KUBECONFIG_DIRECTORY}\" ..."
+      rmdir "${CLUSTERS_KUBECONFIG_DIRECTORY}"
+    fi
+  else
+    echo "*** Cluster \"${CLUSTER_FQDN}\" does not exist..."
+  fi
+}
+
+usage() {
+  echo "*** Usage: $0 {create|delete}"
+  exit 1
+}
+
+: "${CLUSTER_FQDN:?Error: CLUSTER_FQDN environment variable is not set!}"
+: "${CLUSTERS_KUBECONFIG_DIRECTORY:?Error: CLUSTERS_KUBECONFIG_DIRECTORY environment variable is not set!}"
+
+if [[ $# -ne 1 ]]; then
+  usage
+fi
+
+case "$1" in
+  create)
+    echo "*** Creating K8s cluster..."
+    create
+    ;;
+  delete)
+    echo "*** Deleting K8s cluster..."
+    delete
+    ;;
+  *)
+    usage
+    ;;
+esac
